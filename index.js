48b4c2df432dfd155b34199a02d6b67376dd2fa2
Ron Richardson <ron.richardson@gmail.com>
2019-06-20 23:05:53 -0400

    initial UI concepts

--- index.js
+++ index.js
@@ -33,8 +33,8 @@ let mainWindow;
 const createMainWindow = async () => {
 	const win = new BrowserWindow({
 		title: app.getName(),
-		width: 600,
-		height: 400
+		width: 1024,
+		height: 768
 	});
 
 	win.on('closed', () => {
@@ -80,6 +80,6 @@ app.on('activate', () => {
 	Menu.setApplicationMenu(menu);
 	mainWindow = await createMainWindow();
 
-	const favoriteAnimal = config.get('favoriteAnimal');
-	mainWindow.webContents.executeJavaScript(`document.querySelector('header p').textContent = 'Your favorite animal is ${favoriteAnimal}'`);
+	// const favoriteAnimal = config.get('favoriteAnimal');
+	// mainWindow.webContents.executeJavaScript(`document.querySelector('section.main').textContent = 'Your favorite animal is ${favoriteAnimal}'`);
 })();
